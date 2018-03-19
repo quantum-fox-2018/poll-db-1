2a7363629c978ce5864071dce3ea8bcfb7d45ad0
David Joshua <davidjosh_87@yahoo.com>
2018-03-19 20:07:33 +0700

    poll-db-1-release-2

---
+++ seed-data-read.js
@@ -0,0 +1,65 @@
+/*jshint esversion:6*/
+
+const fs = require('fs');
+const sqlite3 = require('sqlite3').verbose();
+
+let db = new sqlite3.Database('polling.db', (err) => {
+  if (err) {
+    return console.error(err.message);
+  }
+  console.log('Connected to the in-memory SQlite database.');
+});
+
+class DataImport {
+  static dataCandidates() {
+    fs.readFile('./politicians.csv', 'utf8', (err, data) => {
+      let dataCandidate = data.trim().split('\n').map(x => x.split(',')).splice(1);
+      for (let i = 0; i < dataCandidate.length; i++) {
+
+        let valueData1 = dataCandidate[i][0];
+        let valueData2 = dataCandidate[i][1];
+        let valueData3 = dataCandidate[i][2];
+        let valueData4 = dataCandidate[i][3];
+        db.run(`INSERT INTO Candidates (candidate_id, candidate_name, party, location, grade_current) VALUES (
+             null, ?, ?, ?, ?)`, valueData1, valueData2, valueData3, valueData4);
+      }
+    });
+  }
+  static dataVoters() {
+    fs.readFile('./voters.csv', 'utf8', (err, data) => {
+      let dataVoters = data.trim().split('\n').map(x => x.split(',')).splice(1);
+      for (let i = 0; i < dataVoters.length; i++) {
+
+        let valueData1 = dataVoters[i][0];
+        let valueData2 = dataVoters[i][1];
+        let valueData3 = dataVoters[i][2];
+        let valueData4 = dataVoters[i][3];
+        db.run(`INSERT INTO Voters (voter_id, first_name, last_name, gender, age) VALUES (
+              null, ?, ?, ?, ?)`, valueData1, valueData2, valueData3, valueData4);
+      }
+    });
+  }
+  static dataVotes() {
+    fs.readFile('./votes.csv', 'utf8', (err, data) => {
+      let dataVotes = data.trim().split('\n').map(x => x.split(',')).splice(1);
+      for (let i = 0; i < dataVotes.length; i++) {
+
+        let valueData1 = dataVotes[i][0];
+        let valueData2 = dataVotes[i][1];
+        db.run(`INSERT INTO Votes (vote_id, voter_id, candidate_id) VALUES (
+              null, ?, ?)`, valueData1, valueData2);
+      }
+    });
+  }
+}
+
+db.close((err) => {
+  if (err) {
+    return console.error(err.message);
+  }
+  console.log('Close the database connection.');
+});
+
+// DataImport.dataCandidates();
+// DataImport.dataVoters();
+// dataImport.dataVotes;
--- setup.js
+++ setup.js
@@ -1,1 +1,40 @@
 //your code here
+
+/*jshint esversion:6*/
+
+const sqlite3 = require('sqlite3').verbose();
+let db = new sqlite3.Database('polling.db', (err) => {
+  if (err) {
+    return console.error(err.message);
+  }
+  console.log('Connected to the in-memory SQlite database.');
+});
+
+db.run(`CREATE TABLE IF NOT EXISTS Voters (
+        voter_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+        first_name TEXT NOT NULL,
+        last_name TEXT NOT NULL,
+        gender VARCHAR(10) NOT NULL,
+        age INTEGER NOT NULL
+      )`);
+db.run(`CREATE TABLE IF NOT EXISTS Candidates (
+        candidate_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+        candidate_name TEXT NOT NULL,
+        party VARCHAR(2) NOT NULL,
+        location VARCHAR(3) NOT NULL,
+        grade_current REAL NOT NULL
+      )`);
+db.run(`CREATE TABLE IF NOT EXISTS Votes (
+        vote_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+        voter_id INTEGER NOT NULL,
+        candidate_id INTEGER NOT NULL,
+        FOREIGN KEY (voter_id) REFERENCES Voters(voter_id),
+        FOREIGN KEY (candidate_id) REFERENCES Candidates(candidate_id)
+      )`);
+
+db.close((err) => {
+  if (err) {
+    return console.error(err.message);
+  }
+  console.log('Close the database connection.');
+});
