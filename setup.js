9ab1037ebc07bd0924b76c889ab2668292380a67
yasirmaulana <yasir.maulana@gmail.com>
2018-03-19 17:43:57 +0700

    week 3, day 1, pool-db-1

--- setup.js
+++ setup.js
@@ -1,1 +1,24 @@
 //your code here
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database('./Pool.db');
+
+db.serialize(function(){
+  db.run(`CREATE TABLE KANDIDAT( Kandidat_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                 Name varchar(50),
+                                 Party varchar(50),
+                                 Location varchar(50),
+                                 Grade_Current varchar(50));`)
+
+  db.run(`CREATE TABLE VOTE_VOTER( Vote_Voter_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                   Kandidat_id varchar(50),
+                                   Voter_id varchar(50));`)
+
+  db.run(`CREATE TABLE VOTER( Voter_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                              First_name varchar(50),
+                              Last_name varchar(50),
+                              Gender varchar(2),
+                              Age varchar(3));`)
+
+})
+
+db.close()
