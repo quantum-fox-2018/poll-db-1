b9b1daff2987a0f7acee7959f8585ac160c73093
cantdocpp <cihuyman95@gmail.com>
2018-03-19 21:17:13 +0700

    first commit

---
+++ seed-data.js
@@ -0,0 +1,60 @@
+const fs = require('fs');
+const politicians = fs.readFileSync('./politicians.csv', 'utf8')
+const votes = fs.readFileSync('./votes.csv', 'utf8');
+const voters = fs.readFileSync('./voters.csv', 'utf8')
+const setup = require('./setup.js');
+const sqlite3 = require('sqlite3').verbose();
+var argv = process.argv;
+let db = new sqlite3.Database('./database.db', (err) => {
+  if (err) {
+    return console.error(err.message);
+  }
+  console.log('Connected to the in-memory SQlite database.');
+});
+
+function showPoliticianTable() {
+  var split = politicians.trim().split('\n');
+  for (var i = 1; i < split.length; i++) {
+    var splitting = split[i].split(',')
+    db.run(`INSERT INTO Politicians (name, party, location, grade_current) VALUES
+    ('${splitting[0]}', '${splitting[1]}', '${splitting[2]}', '${splitting[3]}')`)
+  }
+}
+
+function showVotersTable() {
+  var split = voters.trim().split('\n');
+  for (var i = 1; i < split.length; i++) {
+    var splitting = split[i].split(',')
+    db.run(`INSERT INTO Voters (first_name, last_name, gender, age) VALUES
+    ('${splitting[0]}', '${splitting[1]}', '${splitting[2]}', ${splitting[3]})`)
+  }
+}
+
+function showVotesTable() {
+  var split = votes.trim().split('\n');
+  for (var i = 1; i < split.length; i++) {
+    var splitting = split[i].split(',')
+    db.run(`INSERT INTO votes (voterID, politicianId) VALUES
+    ('${splitting[0]}', '${splitting[1]}')`)
+  }
+}
+
+function updateData(first_name, last_name, gender, age) {
+  db.run(`UPDATE voters
+         SET first_name = '${first_name}',
+         last_name = '${last_name}',
+         gender = '${gender}',
+         age = '${age}'
+         WHERE id = 3`)
+}
+
+function deleteData() {
+  db.run(`DELETE FROM voters
+      WHERE id = 2`)
+}
+
+// updateData('asdsa', 'werwew', 'm', '23')
+deleteData()
+// showVotesTable()
+// showPoliticianTable()
+// showVotersTable()
--- setup.js
+++ setup.js
@@ -1,1 +1,30 @@
-//your code here
+const fs = require('fs');
+const sqlite3 = require('sqlite3').verbose();
+let db = new sqlite3.Database('./database.db', (err) => {
+  if (err) {
+    return console.error(err.message);
+  }
+  console.log('Connected to the in-memory SQlite database.');
+});
+
+db.run(`CREATE TABLE IF NOT EXISTS Politicians
+          (id INTEGER PRIMARY KEY AUTOINCREMENT, name VARCHAR(50),
+          party VARCHAR(50), location VARCHAR(2),
+          grade_current VARCHAR(25))`)
+
+db.run(`CREATE TABLE IF NOT EXISTS Voters
+        (id INTEGER PRIMARY KEY AUTOINCREMENT, first_name VARCHAR(50),
+        last_name VARCHAR(50), gender VARCHAR(2), age INT(3))
+        `)
+
+db.run(`CREATE TABLE IF NOT EXISTS Votes
+      (id INTEGER PRIMARY KEY AUTOINCREMENT, voterID VARCHAR(3),
+      politicianId VARCHAR(3))
+      `)
+
+db.close((err) => {
+  if (err) {
+    return console.error(err.message);
+  }
+  console.log('Close the database connection.');
+});
